46d6e635009d9148e6444b9eac4499c0d566b93d
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-25 12:58:19 -0400

    feature: search input

--- src/components/TrainersIndex.js
+++ src/components/TrainersIndex.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import TrainerItem from './TrainerItem';
+import styles from './TrainersIndex.module.css';
 
 const TrainersIndex = () => (
   <div className="trainersContainer">
-    <form id="trainersSearch" onSubmit={e => e.preventDefault()}>
-      <label htmlFor="trainersInput">
-        <i className="fas fa-search" />
-        <input type="text" id="trainersInput" />
+    <form
+      className={styles.trainersSearch}
+      id="searchForm"
+      onSubmit={e => e.preventDefault()}
+    >
+      <label htmlFor="trainersInput" className={styles.searchLabel}>
+        <span>Search</span>
+        <i className={`fas fa-search ${styles.searchIcon}`} aria-hidden="true" />
+        <input
+          type="text"
+          name="trainersInput"
+          className={styles.searchInput}
+          onFocus={() => window.document.getElementById('searchForm').classList.add(styles.searchFormExpanded)}
+          onBlur={() => window.document.getElementById('searchForm').classList.remove(styles.searchFormExpanded)}
+          placeholder="Looking for a specific trainer?"
+        />
       </label>
     </form>
     <ul id="sliderIndicators">
