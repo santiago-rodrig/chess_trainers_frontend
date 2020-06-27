2596bd4b9a90d1cfc9d975ce26c7bc0514227b5e
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-27 11:45:16 -0400

    feature: sidebar that slides

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import TrainersIndex from '../containers/TrainersIndex';
 import Navbar from './Navbar';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <>
-      <Navbar />
       <Router>
+        <Navbar />
+        <Sidebar />
         <Switch>
           <Route exact path="/">
             <TrainersIndex />
---
+++ src/components/Sidebar.js
@@ -0,0 +1,87 @@
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Sidebar.module.css';
+import './Sidebar.css';
+
+const Sidebar = () => {
+  const sidebarRef = useRef();
+
+  const handleItemClick = e => {
+    const items = window.document.getElementsByClassName('navItem');
+
+    for (let i = 0; i < items.length; i += 1) {
+      if (items[i].classList.contains('navItem-active')) {
+        items[i].classList.remove('navItem-active');
+        break;
+      }
+    }
+
+    e.target.parentNode.classList.add('navItem-active');
+  };
+
+  const handleDimmerClick = e => {
+    sidebarRef.current.classList.add(styles.sidebarHidden);
+    e.target.style.display = 'none';
+  };
+
+  return (
+    <>
+      <div className={styles.dimmer} onClick={handleDimmerClick} />
+      <aside className={styles.sidebar} ref={sidebarRef}>
+        <h1 className={styles.logo}>
+          chess trainers
+        </h1>
+        <nav className={styles.navBox}>
+          <ul className={styles.navList}>
+            <li className="navItem navItem-active">
+              <Link to="/" onClick={handleItemClick}>trainers</Link>
+            </li>
+            <li className="navItem">
+              <Link to="/" onClick={handleItemClick}>appointments</Link>
+            </li>
+            <li className="navItem">
+              <Link to="/" onClick={handleItemClick}>account</Link>
+            </li>
+          </ul>
+        </nav>
+        <footer className={styles.sidebarFooter}>
+          <ul className={styles.socialMediaList}>
+            <li className={styles.socialMediaItem}>
+              <a
+                href="http://example.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-twitter" />
+              </a>
+            </li>
+            <li className={styles.socialMediaItem}>
+              <a
+                href="http://example.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li className={styles.socialMediaItem}>
+              <a
+                href="http://example.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-pinterest-p"></i>
+              </a>
+            </li>
+          </ul>
+          <p className={styles.copyright}>
+            Copyright &copy; Santiago Rodríguez. All rights reserved.
+          </p>
+        </footer>
+      </aside>
+    </>
+  );
+};
+
+export default Sidebar;
+
