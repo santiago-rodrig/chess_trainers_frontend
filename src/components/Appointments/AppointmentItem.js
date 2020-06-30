fa9e078442dd059c7909cf43c1a38138cd3d9a6f
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-30 18:25:36 -0400

    feature: locale date-time string for appointments

--- src/components/Appointments/AppointmentItem.js
+++ src/components/Appointments/AppointmentItem.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const AppointmentItem = ({ appointment, className }) => (
-  <li className={className}>
-    <h2>{appointment.trainer}</h2>
-    <p>
-      <strong>Creation date:</strong>
-      {' '}
-      {appointment.created_at}
-    </p>
-    <p>
-      <strong>Status:</strong>
-      {' '}
-      {appointment.status}
-    </p>
-  </li>
-);
+const AppointmentItem = ({ appointment, className }) => {
+  const creationDate = new Date(Date.parse(appointment.created_at));
+  return (
+      <li className={className}>
+        <h2>{appointment.trainer}</h2>
+        <p>
+          <strong>Creation date:</strong>
+          {' '}
+          {creationDate.toLocaleString()}
+        </p>
+        <p>
+          <strong>Status:</strong>
+          {' '}
+          {appointment.status}
+        </p>
+      </li>
+  );
+};
 
 AppointmentItem.propTypes = {
   appointment: PropTypes.exact({
