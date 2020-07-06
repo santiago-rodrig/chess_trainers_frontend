c1b95c1002df8bd85835599ee9c42523b5f8741f
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-06 18:30:44 -0400

    feature: confirm button to appointments filter

--- src/components/Appointments/FilterModal.js
+++ src/components/Appointments/FilterModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './FilterModal.module.css';
 import SearchForm from './SearchForm';
@@ -13,13 +13,67 @@ const FilterModal = ({
   setSuccessStatus,
   setFailedStatus,
 }) => {
+  const [previousTrainerNameFilter, setPreviousTrainerNameFilter] = useState(trainerNameFilter);
+  const [previousPendingStatus, setPreviousPendingStatus] = useState(status[0]);
+  const [previousSuccessStatus, setPreviousSuccessStatus] = useState(status[1]);
+  const [previousFailedStatus, setPreviousFailedStatus] = useState(status[2]);
+
+  const resetFilters = () => {
+    setTrainerNameFilter(previousTrainerNameFilter);
+    setPendingStatus(previousPendingStatus);
+    setSuccessStatus(previousSuccessStatus);
+    setFailedStatus(previousFailedStatus);
+  };
+
   const closeHandleClick = () => {
+    resetFilters();
+    setDisplaying(false);
+  };
+
+  const handleSubmit = () => {
+    setPreviousTrainerNameFilter(trainerNameFilter);
+    setPreviousPendingStatus(status[0]);
+    setPreviousSuccessStatus(status[1]);
+    setPreviousFailedStatus(status[2]);
     resetCallback();
     setDisplaying(false);
   };
 
+  const handleTrainerNameChange = e => setTrainerNameFilter(e.target.value);
+
+  const handlePendingChange = e => {
+    if (status[0] === '1') {
+      e.target.checked = false;
+      setPendingStatus('0');
+    } else {
+      e.target.checked = true;
+      setPendingStatus('1');
+    }
+  };
+
+  const handleSuccessChange = e => {
+    if (status[1] === '1') {
+      e.target.checked = false;
+      setSuccessStatus('0');
+    } else {
+      e.target.checked = true;
+      setSuccessStatus('1');
+    }
+  };
+
+  const handleFailedChange = e => {
+    if (status[2] === '1') {
+      e.target.checked = false;
+      setFailedStatus('0');
+    } else {
+      e.target.checked = true;
+      setFailedStatus('1');
+    }
+  };
+
+
   return (
-    <>
+    <fragment>
       <div className={styles.dimmer} />
       <div className={styles.filterModal}>
         <button
@@ -31,14 +85,15 @@ const FilterModal = ({
         </button>
         <SearchForm
           trainerNameFilter={trainerNameFilter}
-          setTrainerNameFilter={setTrainerNameFilter}
-          setPendingStatus={setPendingStatus}
-          setSuccessStatus={setSuccessStatus}
-          setFailedStatus={setFailedStatus}
+          handleTrainerNameChange={handleTrainerNameChange}
+          handlePendingChange={handlePendingChange}
+          handleSuccessChange={handleSuccessChange}
+          handleFailedChange={handleFailedChange}
           status={status}
+          handleSubmit={handleSubmit}
         />
       </div>
-    </>
+    </fragment>
   );
 };
 
--- src/components/Appointments/SearchForm.js
+++ src/components/Appointments/SearchForm.js
@@ -9,89 +9,61 @@ const SearchForm = ({
   setPendingStatus,
   setSuccessStatus,
   setFailedStatus,
-}) => {
-  const handleTrainerNameChange = e => setTrainerNameFilter(e.target.value);
-
-  const handlePendingChange = e => {
-    if (status[0] === '1') {
-      e.target.checked = false;
-      setPendingStatus('0');
-    } else {
-      e.target.checked = true;
-      setPendingStatus('1');
-    }
-  };
-
-  const handleSuccessChange = e => {
-    if (status[1] === '1') {
-      e.target.checked = false;
-      setSuccessStatus('0');
-    } else {
-      e.target.checked = true;
-      setSuccessStatus('1');
-    }
-  };
-
-  const handleFailedChange = e => {
-    if (status[2] === '1') {
-      e.target.checked = false;
-      setFailedStatus('0');
-    } else {
-      e.target.checked = true;
-      setFailedStatus('1');
-    }
-  };
-
-  return (
-    <form
-      className={styles.trainersSearch}
-      onSubmit={e => e.preventDefault()}
-    >
-      <label htmlFor="trainersInput" className={styles.searchLabel}>
-        <span className={styles.labelHeading}>Trainer name</span>
+  handleSubmit,
+  handleTrainerNameChange,
+  handlePendingChange,
+  handleSuccessChange,
+  handleFailedChange,
+}) => (
+  <form
+    className={styles.trainersSearch}
+    onSubmit={e => e.preventDefault()}
+  >
+    <label htmlFor="trainersInput" className={styles.searchLabel}>
+      <span className={styles.labelHeading}>Trainer name</span>
+      <input
+        type="text"
+        name="trainersInput"
+        placeholder="pedro ruiz"
+        value={trainerNameFilter}
+        onChange={handleTrainerNameChange}
+      />
+    </label>
+    <div className={styles.expertiseBox}>
+      <div className={styles.labelHeading}>
+        Status
+      </div>
+      <label htmlFor="pending" className={styles.expertise}>
+        Pending
         <input
-          type="text"
-          name="trainersInput"
-          placeholder="pedro ruiz"
-          value={trainerNameFilter}
-          onChange={handleTrainerNameChange}
+          type="checkbox"
+          name="pending"
+          defaultChecked={status[0] === '1'}
+          onChange={handlePendingChange}
         />
       </label>
-      <div className={styles.expertiseBox}>
-        <div className={styles.labelHeading}>
-          Status
-        </div>
-        <label htmlFor="pending" className={styles.expertise}>
-          Pending
-          <input
-            type="checkbox"
-            name="pending"
-            defaultChecked={status[0] === '1'}
-            onChange={handlePendingChange}
-          />
-        </label>
-        <label htmlFor="success" className={styles.expertise}>
-          Success
-          <input
-            type="checkbox"
-            name="success"
-            defaultChecked={status[1] === '1'}
-            onChange={handleSuccessChange}
-          />
-        </label>
-        <label htmlFor="failed" className={styles.expertise}>
-          Failed
-          <input
-            type="checkbox"
-            name="failed"
-            defaultChecked={status[2] === '1'}
-            onChange={handleFailedChange}
-          />
-        </label>
-      </div>
-    </form>
-  );
-};
+      <label htmlFor="success" className={styles.expertise}>
+        Success
+        <input
+          type="checkbox"
+          name="success"
+          defaultChecked={status[1] === '1'}
+          onChange={handleSuccessChange}
+        />
+      </label>
+      <label htmlFor="failed" className={styles.expertise}>
+        Failed
+        <input
+          type="checkbox"
+          name="failed"
+          defaultChecked={status[2] === '1'}
+          onChange={handleFailedChange}
+        />
+      </label>
+    </div>
+    <button type="button" onClick={handleSubmit}>filter</button>
+  </form>
+);
 
 SearchForm.propTypes = {
   trainerNameFilter: PropTypes.string.isRequired,
@@ -100,6 +72,7 @@ SearchForm.propTypes = {
   setPendingStatus: PropTypes.func.isRequired,
   setSuccessStatus: PropTypes.func.isRequired,
   setFailedStatus: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
 };
 
 export default SearchForm;
